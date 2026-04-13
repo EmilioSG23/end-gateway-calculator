461159809341afc5232bb10bf50c787218a8a641
EmilioSG23 <emiliosaenz2001@gmail.com>
2026-04-12 23:24:44 -0500

    feat: add END_GATEWAYS constant with predefined coordinates

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,27 @@
+import type { Coords } from "./types/Coords";
+
 export const MAX_DISTANCE = 1024;
 export const MIN_DISTANCE = 768;
+
+export const END_GATEWAYS: Coords[] = [
+	{ x: 96, z: 0 },
+	{ x: 91, z: 30 },
+	{ x: 78, z: 56 },
+	{ x: 56, z: 78 },
+	{ x: 30, z: 91 },
+	{ x: 0, z: 96 },
+	{ x: -30, z: 91 },
+	{ x: -56, z: 78 },
+	{ x: -78, z: 56 },
+	{ x: -91, z: 30 },
+	{ x: -96, z: 0 },
+	{ x: -91, z: -30 },
+	{ x: -78, z: -56 },
+	{ x: -56, z: -78 },
+	{ x: -30, z: -91 },
+	{ x: 0, z: -96 },
+	{ x: 30, z: -91 },
+	{ x: 56, z: -78 },
+	{ x: 78, z: -56 },
+	{ x: 91, z: -30 },
+];
