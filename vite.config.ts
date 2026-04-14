8f34298776d706fdbb41ec86745a864a28661b52
EmilioSG23 <emiliosaenz2001@gmail.com>
2026-04-13 22:46:42 -0500

    fix: update base URL in Vite configuration to match project path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.BASE_URL || "/",
+	base: process.env.BASE_URL || "/end-gateway-calculator/",
 	plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 	resolve: {
 		alias: {
