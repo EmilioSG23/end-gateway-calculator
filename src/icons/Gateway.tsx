dbcaddba617d580deb029b5e760ef2edc2eeaedc
EmilioSG23 <emiliosaenz2001@gmail.com>
2026-04-13 22:51:10 -0500

    fix: update image source in GatewayIcon component to use BASE_URL

--- src/icons/Gateway.tsx
+++ src/icons/Gateway.tsx
@@ -10,7 +10,7 @@ export function GatewayIcon({ className }: { className?: string }) {
 		<div className={`inline-flex items-center justify-center ${className ?? ""}`}>
 			<div className="relative size-16 flex items-center justify-center ">
 				<img
-					src="/end_gateway.png"
+					src={import.meta.env.BASE_URL + "end_gateway.png"}
 					alt="gateway"
 					className="relative z-10 size-12 object-contain rounded-full p-0.5"
 				/>
